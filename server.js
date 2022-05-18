4cfb56b6496b2a41601049f25c88309644e0ba51
alternadiva <asomeonelse@gmail.com>
2022-05-18 16:50:00 +0100

    setup server file

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 const express = require("express");
 
+const home = require("./routes/home.js");
+
 const server = express();
+
+const bodyHandler = express.urlencoded({ extended: false });
+
+server.use(bodyHandler);
+
+server.get("/", home.get);
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
