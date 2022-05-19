036a5a9bb043dc99dd27b7c1304265365dabd148
joe <joe.dev.public@gmail.com>
2022-05-19 11:41:40 +0100

    Add template layoutmodule

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,17 @@
 const db = require("../database/connection.js");
+const layout = require("../layout.js");
 
 function get(request, response) {
-  db.query("SELECT 'hello everyone' AS test").then((result) => {
-    response.send(result.rows);
+  db.query("SELECT 'hello everyone' AS bananas").then((result) => {
+    const html = layout(
+      "Written Tomatoes",
+      /*html*/ `
+        <h1>Test</h1>
+        <p>${result.rows[0].bananas}</p>
+    `
+    );
+
+    response.send(html);
   });
 }
 
