83da49366c1b4dff2f7fac598146a7d9184056f6
joe <joe.dev.public@gmail.com>
2022-05-19 10:24:42 +0100

    Fix SSL for Heroku Postgres connection

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,9 @@
 const db = require("../database/connection.js");
 
 function get(request, response) {
-    response.send("hello world")
-   
+  db.query("SELECT 'hello everyone' AS test").then((result) => {
+    response.send(result.rows);
+  });
 }
 
 module.exports = { get };
