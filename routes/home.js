04741316d03a5e00a2de749778d461a0dd037c25
alternadiva <asomeonelse@gmail.com>
2022-05-19 12:25:23 +0100

    add reviews thread

--- routes/home.js
+++ routes/home.js
@@ -2,12 +2,24 @@ const db = require("../database/connection.js");
 const layout = require("../layout.js");
 
 function get(request, response) {
-  db.query("SELECT 'hello everyone' AS bananas").then((result) => {
+  db.query("SELECT * FROM reviews").then((result) => {
+    const reviewsObjects = result.rows;
+    const allReviews = reviewsObjects.map((review) => {
+      return `
+      <div>
+        <h2>${review.username}</h2>
+        <h3>${review.title}</h3>
+        <h4>${review.author}</h4>
+        <p>${review.rating}</p>
+        <p>${review.textcontent}</p>
+      </div>
+      `;
+    });
     const html = layout(
       "Written Tomatoes",
       /*html*/ `
-        <h1>Test</h1>
-        <p>${result.rows[0].bananas}</p>
+        <h1>Written Tomatoes Reviews</h1>
+        <div>${allReviews}</div>
     `
     );
 
